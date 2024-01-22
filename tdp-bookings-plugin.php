1c8b008e5aeace06d7c08f719efa84ccc8049ec6
oskarvedel <oskar.vedel@gmail.com>
2024-01-22 14:04:48 +0100

    add booking confirmation page

---
+++ tdp-bookings-plugin.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Plugin Name: tdp-bookings
+ * Version: 1.0
+ */
+
+function custom_rewrite_booking_confirmation()
+{
+    add_rewrite_rule('^reservation/confirmation/([0-9]+)/?$', 'index.php?pagename=reservation-confirmation&booking_id=$matches[1]', 'top');
+}
+add_action('init', 'custom_rewrite_booking_confirmation');
+
+function custom_query_vars_filter($vars)
+{
+    $vars[] .= 'booking_id';
+    return $vars;
+}
+add_filter('query_vars', 'custom_query_vars_filter');
+
+function custom_template_include($template)
+{
+    if (get_query_var('booking_id')) {
+        $new_template = locate_template(array('reservation-confirmation.php'));
+        if ('' != $new_template) {
+            return $new_template;
+        }
+    }
+    return $template;
+}
+add_filter('template_include', 'custom_template_include', 99);
